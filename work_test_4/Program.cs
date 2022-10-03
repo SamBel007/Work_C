41993d7099fcb5b45c63d6e8a2ca018488be15c9
Alex Beloglazov <barber004@mail.ru>
2022-10-03 22:50:54 +0300

    test_4

---
+++ work_test_4/Program.cs
@@ -0,0 +1,14 @@
+﻿//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//5 -> 2, 4
+//8 -> 2, 4, 6, 8
+
+
+Console.WriteLine("Write a number N : ");
+int a = int.Parse(Console.ReadLine());
+int Num = 2;
+while ( Num <= a)
+{
+    Console.Write(Num);
+    Console.Write(", ");
+    Num += 2;
+}
